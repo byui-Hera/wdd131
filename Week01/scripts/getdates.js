d2e59eed31518ca0cc8fe240e95f2442c5ac7875
byui-Hera <herahysi@gmail.com>
2025-03-04 20:07:51 +0100

    save the changes

--- Week01/scripts/getdates.js
+++ Week01/scripts/getdates.js
@@ -1,14 +1,3 @@
-// JavaScript to dynamically update the year and last modified date
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Set the current year dynamically
-    const currentYearElement = document.getElementById('currentYear');
-    const currentYear = new Date().getFullYear();
-    currentYearElement.textContent = currentYear;
-
-
-    // Set the last modified date dynamically
-    const lastModifiedElement = document.getElementById('lastModified');
-    const lastModifiedDate = document.lastModified;
-    lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
-});
+let d = new Date();
+document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()}`;
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
