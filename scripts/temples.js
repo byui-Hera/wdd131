3928ac1b5454b60d177e7b49c55b174cc4864001
byui-Hera <herahysi@gmail.com>
2025-03-11 18:56:16 +0100

    save the changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,9 @@
 // Set the current year in the footer
 let d = new Date();
-document.getElementById("currentYear").innerHTML = `\u00A9${d.getFullYear()}`;
+const currentYearElement = document.getElementById("currentYear");
+if (currentYearElement) {
+    currentYearElement.innerHTML = `\u00A9${d.getFullYear()}`;
+}
 
 // Set the last modified date in the footer
 document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
